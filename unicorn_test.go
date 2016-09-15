abb89e41cda35118e00162f2f8de32ecaaed3cc4
hq <hq_cml@163.com>
2016-09-15 11:20:49 +0800

    NewTcpServer

--- unicorn_test.go
+++ unicorn_test.go
@@ -3,10 +3,21 @@ package main
 import (
     "testing"
     "runtime"
+    "github.com/hq-cml/unicorn-go/unicorn"
+    "github.com/hq-cml/unicorn-go/plugin"
 )
 
 func TestStart(t *testing.T) {
     runtime.GOMAXPROCS(runtime.NumCPU())
 
-
+    //初始化Server
+    server := plugin.NewTcpServer()
+    defer server.Close() //注册关闭
+    addr := "127.0.0.1:9527"
+    t.Logf("Startup Tcp Server(%s)..\n", addr)
+    err := server.Listen(addr)
+    if err != nil {
+        t.Fatalf("TCP Server startup failing! (addr=%s)!\n", addr)
+        t.FailNow() //结束！
+    }
 }
