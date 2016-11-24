27ca13f4efe72bd40e15a8f2da1c120ce483f6f7
hq <hq_cml@163.com>
2016-11-24 22:32:13 +0800

    refact

--- plugin/tcp_equation.go
+++ plugin/tcp_equation.go
@@ -13,10 +13,6 @@ import (
     "net"
     "bytes"
     "bufio"
-    "strconv"
-    "errors"
-    "sync"
-    "runtime"
     "math/rand"
 )
 
@@ -165,7 +161,6 @@ func read(conn net.Conn, delim byte) ([]byte, error) {
     return buffer.Bytes(), nil
 }
 
-/**************************配套服务端的逻辑********************/
 func op(operands []int, operator string) int {
     var result int
     switch {
@@ -203,112 +198,4 @@ func op(operands []int, operator string) int {
         }
     }
     return result
-}
-
-func genFormula(operands []int, operator string, result int, equal bool) string {
-    var buff bytes.Buffer
-    n := len(operands)
-    for i := 0; i < n; i++ {
-        if i > 0 {
-            buff.WriteString(" ")
-            buff.WriteString(operator)
-            buff.WriteString(" ")
-        }
-
-        buff.WriteString(strconv.Itoa(operands[i]))
-    }
-    if equal {
-        buff.WriteString(" = ")
-    } else {
-        buff.WriteString(" != ")
-    }
-    buff.WriteString(strconv.Itoa(result))
-    return buff.String()
-}
-
-func reqHandler(conn net.Conn) {
-    var errMsg string
-    var sresp ServerEquationResp
-    req, err := read(conn, DELIM)
-    if err != nil {
-        errMsg = fmt.Sprintf("Server: Req Read Error: %s", err)
-    } else {
-        var sreq ServerEquationReq
-        err := json.Unmarshal(req, &sreq)
-        if err != nil {
-            errMsg = fmt.Sprintf("Server: Req Unmarshal Error: %s", err)
-        } else {
-            sresp.Id = sreq.Id
-            sresp.Result = op(sreq.Operands, sreq.Operator)
-            sresp.Formula =
-            genFormula(sreq.Operands, sreq.Operator, sresp.Result, true)
-        }
-    }
-    if errMsg != "" {
-        sresp.Err = errors.New(errMsg)
-    }
-    bytes, err := json.Marshal(sresp)
-    if err != nil {
-        fmt.Errorf("Server: Resp Marshal Error: %s", err)
-    }
-    _, err = write(conn, bytes, DELIM)
-    if err != nil {
-        fmt.Errorf("Server: Resp Write error: %s", err)
-    }
-}
-
-type TcpServer struct {
-    listener net.Listener
-    active   bool
-    lock     *sync.Mutex
-}
-
-func (self *TcpServer) init(addr string) error {
-    self.lock.Lock()
-    defer self.lock.Unlock()
-    if self.active {
-        return nil
-    }
-    ln, err := net.Listen("tcp", addr)
-    if err != nil {
-        return err
-    }
-    self.listener = ln
-    self.active = true
-    return nil
-}
-
-func (self *TcpServer) Listen(addr string) error {
-    err := self.init(addr)
-    if err != nil {
-        return err
-    }
-    go func(active *bool) {
-        for {
-            conn, err := self.listener.Accept()
-            if err != nil {
-                fmt.Errorf("Server: Request Acception Error: %s\n", err)
-                continue
-            }
-            go reqHandler(conn)
-            runtime.Gosched()
-        }
-    }(&self.active)
-    return nil
-}
-
-func (self *TcpServer) Close() bool {
-    self.lock.Lock()
-    defer self.lock.Unlock()
-    if self.active {
-        self.listener.Close()
-        self.active = false
-        return true
-    } else {
-        return false
-    }
-}
-
-func NewTcpServer() *TcpServer {
-    return &TcpServer{lock: new(sync.Mutex)}
 }
